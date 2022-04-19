1bf641146ca598d79e7d4697ffefc2f67b48d186
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2022-04-18 19:47:47 -0600

    examples page

---
+++ packages/use-ammo-examples/Readme.md
@@ -0,0 +1,1 @@
+Site copied from https://github.com/pmndrs/use-cannon/tree/master/packages/react-three-cannon-examples
---
+++ packages/use-ammo-examples/src/demos/index.ts
@@ -0,0 +1,22 @@
+import { lazy } from 'react'
+
+const fileDemos = [
+  'Cubes',
+] as const
+
+const folderDemos = [] as const
+
+export const demoList = [...folderDemos, ...fileDemos] as const
+
+export type Demo = typeof demoList[number]
+export const isDemo = (v: unknown): v is Demo => demoList.includes(v as Demo)
+
+type FolderDemo = typeof folderDemos[number]
+const isFolderDemo = (v: unknown): v is FolderDemo => folderDemos.includes(v as FolderDemo)
+
+export const demos = demoList.reduce((o, demo) => {
+  o[demo] = {
+    Component: lazy(() => import(isFolderDemo(demo) ? `./${demo}/index.tsx` : `./demo-${demo}.tsx`)),
+  }
+  return o
+}, {} as Record<Demo, { Component: React.LazyExoticComponent<React.ComponentType> }>)
